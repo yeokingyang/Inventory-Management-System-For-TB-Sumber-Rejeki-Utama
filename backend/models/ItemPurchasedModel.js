c553d0029f726a09a8d7ba746502f8845b0bf692
yeokingyang <yeokingyang@yahoo.com>
2023-02-22 23:19:53 +0800

    doing login system auth, and modeling items sql

--- backend/models/ItemPurchasedModel.js
+++ backend/models/ItemPurchasedModel.js
@@ -1,25 +1,22 @@
 import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
-import Users from "./UserModel.js";
 
 const {DataTypes} = Sequelize;
 
-const Items = db.define('items',{
+const ItemsPurchased = db.define('items',{
     iuid: {
         type:DataTypes.STRING,
         unique: true,
         allowNull: false,
         validate:{
             notEmpty: true,
-            len: [3, 100]
         }
     },
     name: {
         type:DataTypes.STRING,
         allowNull: false,
         validate:{
-            notEmpty: true,
-            len: [3, 100]
+            notEmpty: true,    
         }
     },
     debit: {
@@ -29,52 +26,30 @@ const Items = db.define('items',{
             notEmpty: true,
         }
     },
-    credit: {
+    totalDebit: {
         type:DataTypes.INTEGER,
         allowNull: false,
         validate:{
             notEmpty: true,
         }
     },
-    quantityReceived: {
+    quantityPurchased: {
         type:DataTypes.INTEGER,
         defaultValue: 0,
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-        }
-    },
-    quantityOnHand: {
-        type:DataTypes.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-        }
-    },
-    quantitySold: {
-        type:DataTypes.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-        validate:{
-            notEmpty: true,
-        }
-    },
-    userId: {
-        type:DataTypes.INTEGER,
         allowNull: false,
         validate:{
             notEmpty: true,
         }
     }
+
 },{
     freezeTableName:true
 });
 
-Users.hasMany(Items);
-Items.belongsTo(Users, {foreignKey:'userId'});
+Items.hasMany(ItemsPurchased);
+ItemsPurchased.belongsTo(Items, {foreignKey:'iuid'});
 
-export default Items;
+export default ItemsPurchased;
 
 //(async()=>{
   // await db.sync();

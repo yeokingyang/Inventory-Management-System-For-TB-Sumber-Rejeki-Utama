88f4d94d872b51e1860dae6f00a14104e714ebc0
yeokingyang <yeokingyang@yahoo.com>
2023-02-25 21:48:26 +0800

    creating frontend for components: sidebar, etc and pages: item layout etc

---
+++ frontend/src/components/userlist.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+
+const Userlist = () => {
+
+    return (
+        <div>
+            <h1 className="title text-4xl font-bold">Users</h1>
+            <h2 className="subtitle">List of Users</h2>
+
+            <table className="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2">No</th>
+                        <th className="px-4 py-2">Name</th>
+                        <th className="px-4 py-2">Email</th>
+                        <th className="px-4 py-2">Role</th>
+                        <th className="px-4 py-2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="border">
+                        <td className="px-4 py-2 border"></td>
+                        <td className="px-4 py-2 border"></td>
+                        <td className="px-4 py-2 border"></td>
+                        <td className="px-4 py-2 border"></td>
+                        <td className="px-4 py-2 border"></td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    );
+};
+
+export default Userlist;

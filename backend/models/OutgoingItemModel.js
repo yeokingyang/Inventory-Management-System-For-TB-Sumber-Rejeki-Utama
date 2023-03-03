32e18beb93d16c00cd32f27000b413a4642d188b
yeokingyang <yeokingyang@yahoo.com>
2023-03-03 22:09:59 +0800

    creating bacekend for outgoingitem and incoming item for purchasing and selling item

---
+++ backend/models/OutgoingItemModel.js
@@ -0,0 +1,69 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Items from "./ItemModel.js";
+
+const { DataTypes } = Sequelize;
+
+const OutgoingItems = db.define('outgoingItems', {
+
+    iuid: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [3, 100]
+        },
+        references: {
+            model: Items,
+            key: 'iuid'
+        }
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    },
+    credit: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    },
+    totalCredit: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    },
+    quantitySold: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    },
+}, {
+    freezeTableName: true,
+});
+
+Items.hasMany(OutgoingItems, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+});
+OutgoingItems.belongsTo(Items, { 
+    foreignKey: 'iuid', 
+    onDelete: 'CASCADE',
+     onUpdate: 'CASCADE' });
+
+export default OutgoingItems;
+
+//(async () => {
+   // console.log('Syncing database...');
+//  await db.sync();
+ //  console.log('Database synced!');
+ // })();
